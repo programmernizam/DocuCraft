b5baa14debf1754d29740925ea4294efe6dfbd96
programmerNizam <mdnizam12109@gmail.com>
2024-09-26 02:41:13 +0600

    Existing Project update

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,16 +1,54 @@
-export default function Search() {
+"use client";
+
+import { useDebounce } from "@/hooks/useDebounce";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import SearchResult from "./SearchResult";
+
+export default function Search({ docs }) {
+  const [searchResult, setSearchResult] = useState([]);
+  const [term, setTerm] = useState("");
+
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setTerm(value);
+    doSearch(value);
+  };
+
+  const doSearch = useDebounce((term) => {
+    const found = docs.filter((doc) => {
+      return doc.title.toLowerCase().includes(term.toLowerCase());
+    });
+    setSearchResult(found);
+  }, 500);
+
+  const closeSearchResult = (e) => {
+    e.preventDefault();
+    router.push(e.target.href);
+    setTerm("");
+  };
   return (
-    <div>
-      Search
-      {/* <div className="relative hidden lg:block lg:max-w-md lg:flex-auto">
+    <>
+      <div className="relative hidden lg:block lg:max-w-md lg:flex-auto">
         <button
           type="button"
           className="focus:[&amp;:not(:focus-visible)]:outline-none hidden h-8 w-full items-center gap-2 rounded-full bg-white pl-2 pr-3 text-sm text-zinc-500 ring-1 ring-zinc-900/10 transition hover:ring-zinc-900/20 dark:bg-white/5 dark:text-zinc-400 dark:ring-inset dark:ring-white/10 dark:hover:ring-white/20 lg:flex"
         >
-          <img src="./assets/icons/search.svg" className="h-5 w-5" />
+          <Image
+            src="/search.svg"
+            className="h-5 w-5"
+            width={50}
+            height={50}
+            alt="searchIcon"
+          />
           <input
             type="text"
+            value={term}
             placeholder="Search..."
+            onChange={handleChange}
             className="flex-1 focus:border-none focus:outline-none"
           />
           <kbd className="ml-auto w-auto text-2xs text-zinc-400 dark:text-zinc-500">
@@ -18,37 +56,10 @@ export default function Search() {
             <kbd className="font-sans">K</kbd>
           </kbd>
         </button>
-         <!-- result card -->
-        <div className="absolute left-0 top-12 z-[999] w-full rounded-md bg-white p-4 shadow">
-          <p className="!text-lg">
-            Showing results for
-            <span className="font-semibold">"keyword":</span>
-          </p>
-          <ul role="list" className="divide-y divide-gray-100 [&>*]:py-2">
-            <li className="">
-              <a className="transition-all hover:text-emerald-600" href="#">
-                How to create a new component in Vue.js
-              </a>
-            </li>
-            <li className="">
-              <a className="transition-all hover:text-emerald-600" href="#">
-                How to create a new component in React.js
-              </a>
-            </li>
-            <li className="">
-              <a className="transition-all hover:text-emerald-600" href="#">
-                Next.js Routing
-              </a>
-            </li>
-            <li className="">
-              <a className="transition-all hover:text-emerald-600" href="#">
-                SSR - What is it?
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-       <!-- Mobile Responsive Header Starts --> 
+        {/* <!-- result card -->
+        
+      </div> */}
+        {/* <!-- Mobile Responsive Header Starts --> 
       <div className="flex items-center gap-5 lg:hidden">
         <button
           type="button"
@@ -64,8 +75,8 @@ export default function Search() {
         <a aria-label="Home" href="/">
           <img src="./assets/icons/logo.svg" alt="Protocol" className="h-6" />
         </a>
-      </div>
-      <--Mobile Responsive Header Ends --> 
+      </div> */}
+        {/* <--Mobile Responsive Header Ends --> 
       <div className="flex items-center gap-5">
         <div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15"></div>
         <div className="flex gap-4">
@@ -84,6 +95,14 @@ export default function Search() {
           </div>
         </div>
       </div> */}
-    </div>
+      </div>
+      {term && term.trim().length > 0 && (
+        <SearchResult
+          results={searchResult}
+          term={term}
+          closeSearchResult={closeSearchResult}
+        />
+      )}
+    </>
   );
 }
